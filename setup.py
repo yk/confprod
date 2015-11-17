1f66b6df2a51afd8ddc5db54f39f55a85c505278
Yannic Kilcher <yannic.kilcher@gmail.com>
2015-11-17 16:25:40 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='confprod',
+    version='0.0.1',
+    packages=['.'],
+    package_dir={'':'confprod'},
+    url='',
+    license='MIT',
+    author='yk',
+    author_email='',
+    description=''
+)
