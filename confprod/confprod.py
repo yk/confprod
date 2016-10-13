89b72dcc3f400e0733139e423cda9231e40a041d
Yannic Kilcher <yannic.kilcher@gmail.com>
2016-10-13 11:11:16 +0200

    allow numpy arrays

--- confprod/confprod.py
+++ confprod/confprod.py
@@ -1,11 +1,12 @@
 import itertools
 import random
 import math
+from numpy import array
 
 
 def _ensure_list(l):
     """ensures that l is list. if not, returns [l]"""
-    return l if isinstance(l, list) else [l]
+    return l if isinstance(l, (list, array)) else [l]
 
 
 def add_to_configurations(configs: list, d: dict):
